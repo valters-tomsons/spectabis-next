fa2744f06fe6392780d3f91eef57c831fe0ff91f
Valters <infinity1188@yahoo.com>
2018-12-25 23:43:00 +0200

    Read game tile size from configuration

--- SpectabisNext/Factories/GameTileFactory.cs
+++ SpectabisNext/Factories/GameTileFactory.cs
@@ -1,18 +1,27 @@
 using SpectabisLib.Models;
 using SpectabisNext.Controls.GameTile;
+using SpectabisNext.Models.Configuration;
 
 namespace SpectabisNext.Factories
 {
     public class GameTileFactory
     {
-        public GameTileFactory()
+        private readonly UIConfiguration _uiConfiguration;
+        public GameTileFactory(UIConfiguration uiConfiguration)
         {
+            _uiConfiguration = uiConfiguration;
 
         }
 
         public GameTileView Create(GameProfile game)
         {
-            var tileView = new GameTileView(game);
+            var sizeModifier = _uiConfiguration.BoxArtSizeModifier;
+
+            var tileView = new GameTileView(game)
+            {
+                Height = _uiConfiguration.BoxArtHeight * sizeModifier,
+                Width = _uiConfiguration.BoxArtWidth * sizeModifier
+            };
             return tileView;
         }
     }
