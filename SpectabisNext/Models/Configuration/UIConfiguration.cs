209409c1d328c7dee5ff377956e86d4ff8c40665
Valters <infinity1188@yahoo.com>
2018-12-26 00:06:52 +0200

    Make background gradient defaults

--- SpectabisNext/Models/Configuration/UIConfiguration.cs
+++ SpectabisNext/Models/Configuration/UIConfiguration.cs
@@ -10,30 +10,38 @@ namespace SpectabisNext.Models.Configuration
             BoxArtHeight = 200;
             BoxArtSizeModifier = 1.2;
 
-            UIBackgroundGradient = new LinearGradientBrush()
-            {
-                StartPoint = new Avalonia.RelativePoint(0.5, 1, 0),
-                EndPoint = new Avalonia.RelativePoint(0.5, 0, 0),
-
-                GradientStops = {
-                new GradientStop()
-                {
-                Color = Color.Parse("#BDBDBD"),
-                Offset = 0
-                },
-
-                new GradientStop()
-                {
-                Color = Color.Parse("#F5F5F5"),
-                Offset = 0.56
-                }
-                }
-            };
+            UIBackgroundGradient = DefaultBackgroundGradient();
         }
 
         public double BoxArtWidth { get; set; }
         public double BoxArtHeight { get; set; }
         public double BoxArtSizeModifier { get; set; }
         public LinearGradientBrush UIBackgroundGradient { get; set; }
+
+        private LinearGradientBrush DefaultBackgroundGradient()
+        {
+            var gradient = new LinearGradientBrush()
+            {
+                StartPoint = new Avalonia.RelativePoint(0.5, 1, 0),
+                EndPoint = new Avalonia.RelativePoint(0.5, 0, 0),
+            };
+
+            var stop1 = new GradientStop()
+            {
+                Color = Color.Parse("#BDBDBD"),
+                Offset = 0
+            };
+
+            var stop2 = new GradientStop()
+            {
+                Color = Color.Parse("#F5F5F5"),
+                Offset = 0.56
+            };
+
+            gradient.GradientStops.Add(stop1);
+            gradient.GradientStops.Add(stop2);
+
+            return gradient;
+        }
     }
 }
