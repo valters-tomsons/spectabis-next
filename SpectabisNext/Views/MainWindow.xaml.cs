64cf10499901de8848f7446d0163c5377637d482
Valters <infinity1188@yahoo.com>
2018-12-27 03:25:23 +0200

    Add titlebar panel

--- SpectabisNext/Views/MainWindow.xaml.cs
+++ SpectabisNext/Views/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
 using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using SpectabisNext.Interfaces;
 using SpectabisNext.Models.Configuration;
 using SpectabisNext.Repositories;
@@ -14,6 +15,7 @@ namespace SpectabisNext.Views
         private readonly UIConfiguration _uiConfiguration;
         private readonly PageRepository _pageRepository;
         private Rectangle Titlebar;
+        private StackPanel TitlebarPanel;
         private ContentControl ContentContainer;
 
         public MainWindow(UIConfiguration uiConfiguration, PageRepository pageRepository)
@@ -24,8 +26,14 @@ namespace SpectabisNext.Views
             InitializeComponent();
             RegisterChildern();
             FillElementColors();
+            GeneratePageIcons();
 
             ContentContainer.Content = _pageRepository.GetAll().First();
+        }
+
+        private void GeneratePageIcons()
+        {
+            TitlebarPanel.Children.Add(new TextBlock(){Text = "+", FontSize = 20, Foreground = new SolidColorBrush(Colors.White)});
         }
 
         private void FillElementColors()
@@ -37,6 +45,7 @@ namespace SpectabisNext.Views
         private void RegisterChildern()
         {
             Titlebar = this.FindControl<Rectangle>("Titlebar");
+            TitlebarPanel = this.FindControl<StackPanel>("TitlebarPanel");
             ContentContainer = this.FindControl<ContentControl>("ContentContainer");
         }
 
