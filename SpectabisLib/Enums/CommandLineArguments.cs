ee799839214d013a37773885abd75d32a46d1f2f
Valters <infinity1188@yahoo.com>
2019-01-03 14:19:31 +0200

    Add PCSX2 command line argument enum

---
+++ SpectabisLib/Enums/CommandLineArguments.cs
@@ -0,0 +1,20 @@
+namespace SpectabisLib.Enums
+{
+    public enum CommandLineArguments
+    {
+        cfgpath,
+        fullscreen,
+        windowed,
+        nogui,
+        nodisc,
+        nohacks,
+        fullboot,
+        gs,
+        pad,
+        spu2,
+        cdvd,
+        usb,
+        fw,
+        dev9
+    }
+}
