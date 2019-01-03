d7173e7430b560b5e57281519877ee791acf77ae
Valters <infinity1188@yahoo.com>
2019-01-03 14:10:24 +0200

    Make HomePath readonly

--- SpectabisLib/Helpers/SystemDirectories.cs
+++ SpectabisLib/Helpers/SystemDirectories.cs
@@ -4,7 +4,7 @@ namespace SpectabisLib
 {
     public static class SystemDirectories
     {
-        public static string HomePath = (Environment.OSVersion.Platform == PlatformID.Unix ||
+        public static readonly string HomePath = (Environment.OSVersion.Platform == PlatformID.Unix ||
                 Environment.OSVersion.Platform == PlatformID.MacOSX) ?
             Environment.GetEnvironmentVariable("HOME") :
             Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
