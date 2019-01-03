330316f04e3ada8132d5a516c0217f23bdb494a7
Valters <infinity1188@yahoo.com>
2019-01-03 14:19:41 +0200

    Add launch option list to game profile

--- SpectabisLib/Models/GameProfile.cs
+++ SpectabisLib/Models/GameProfile.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using SpectabisLib.Enums;
+
 namespace SpectabisLib.Models
 {
     public class GameProfile
@@ -7,5 +10,6 @@ namespace SpectabisLib.Models
         public string SerialNumber { get; set; }
         public string FilePath { get; set; }
         public string BoxArtPath { get; set; }
+        public List<CommandLineArguments> LaunchOptions { get; set; }
     }
 }
