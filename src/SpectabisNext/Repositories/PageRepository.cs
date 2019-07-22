701cb059e9054d10e45a222ace63d971d1ff03dc
FaithLV <valters.tomsons@protonmail.com>
2019-07-22 11:20:22 +0300

    Do not add ReloadOnNavigation pages to repository

--- src/SpectabisNext/Repositories/PageRepository.cs
+++ src/SpectabisNext/Repositories/PageRepository.cs
@@ -21,14 +21,21 @@ namespace SpectabisNext.Repositories
 
         public IEnumerable<Page> All => Pages;
 
+        /// <summary>
+        /// Return an instance of a page from repository. If page does not exist yet, one is created.
+        /// </summary>
         public Page GetPage<T>()
         {
             var page = Pages.SingleOrDefault(x => x.GetType() == typeof(T)); 
 
-            if(page == null || page.ReloadOnNavigation)
+            if(page == null)
             {
                 page = _pageFactory.Create<T>();
-                Pages.Add(page);
+
+                if(!page.ReloadOnNavigation)
+                {
+                    Pages.Add(page);
+                }
             }
 
             return page;
