1f18f3f08ed7a374ac07f9ff7eb25199c88978d0
Valters <valters.tomsons@protonmail.com>
2019-09-02 01:13:17 +0300

    Add code for PCSX2 launcher service

--- src/SpectabisLib/Services/GameLauncherPCSX2.cs
+++ src/SpectabisLib/Services/GameLauncherPCSX2.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using SpectabisLib.Enums;
+using SpectabisLib.Helpers;
 using SpectabisLib.Interfaces;
 using SpectabisLib.Models;
 
@@ -7,19 +9,44 @@ namespace SpectabisLib.Services
     public class GameLauncherPCSX2 : IGameLauncher
     {
         public string EmulatorPath { get; set; }
-        public GameLauncherPCSX2()
+        private Process EmulatorProcess { get; set; }
+        public GameLauncherPCSX2(string EmulatorPath)
         {
 
         }
 
         public Process Launch(GameProfile game)
         {
-            throw new System.NotImplementedException();
+            EmulatorProcess = CreateEmulatorProcess(game);
+            EmulatorProcess.Start();
+
+            return EmulatorProcess;
         }
 
         public void BeginShutdown()
         {
-            throw new System.NotImplementedException();
+            KillEmulatorProcess();
         }
+
+        private void KillEmulatorProcess()
+        {
+            EmulatorProcess.Kill();
+        }
+
+        private Process CreateEmulatorProcess(GameProfile gameProfile)
+        {
+            var process = new Process();
+
+            var launchArguments = EmulatorOptionsParser.ConvertToLaunchArguments(gameProfile.LaunchOptions);
+            var romArgument = EmulatorOptionsParser.RomPathToArgument(gameProfile.FilePath);
+            var fullArguments = $"{launchArguments} {romArgument}";
+
+            process.StartInfo.FileName = EmulatorPath;
+            process.StartInfo.Arguments = fullArguments;
+            process.EnableRaisingEvents = true;
+
+            return process;
+        }
+
     }
 }
