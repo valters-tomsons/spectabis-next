b56dfbb214169274ada1cc09d8dcfaa23ed6a1ca
Valters <valters.tomsons@protonmail.com>
2019-09-02 00:49:01 +0300

    Add tests for Emulator Options Enum parser

---
+++ src/SpectabisTests/CoreLibraryTest/Helpers/OptionsParserChecker.cs
@@ -0,0 +1,31 @@
+using System;
+using SpectabisLib.Enums;
+using SpectabisLib.Helpers;
+using Xunit;
+
+namespace CoreLibraryTest
+{
+    public class OptionsParserChecker
+    {
+        [Fact]
+        public void CheckIfOptionsAreParsedCorrectAsArguments()
+        {
+            EmulatorLaunchOptions options = EmulatorLaunchOptions.Fullboot | EmulatorLaunchOptions.Nohacks;
+
+            var arguments = EmulatorOptionsParser.ConvertToLaunchArguments(options);
+
+            Assert.True(arguments.Contains("--fullboot"));
+            Assert.True(arguments.Contains("--nohacks"));
+        }
+
+        [Fact]
+        public void CheckIfArgumentOptionsAreSplitWithSpaces()
+        {
+            EmulatorLaunchOptions options = EmulatorLaunchOptions.Fullboot | EmulatorLaunchOptions.Nohacks;
+
+            var arguments = EmulatorOptionsParser.ConvertToLaunchArguments(options);
+
+            Assert.True(arguments.Contains("--nohacks --fullboot"));
+        }
+    }
+}
