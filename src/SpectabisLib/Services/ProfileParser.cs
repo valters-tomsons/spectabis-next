39c58895233bdaff7f9c63cff6e5ea27c6f71f69
Valters <valters.tomsons@protonmail.com>
2019-09-05 00:46:55 +0300

    Add ProfileParser

---
+++ src/SpectabisLib/Services/ProfileParser.cs
@@ -0,0 +1,18 @@
+using System;
+using SpectabisLib.Models;
+
+namespace SpectabisLib.Services
+{
+    public class ProfileParser
+    {
+        public ProfileParser()
+        {
+
+        }
+
+        public GameProfile ParseFromJson(string json)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
