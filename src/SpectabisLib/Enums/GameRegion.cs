209154798df2981a6effb4b303c5311da606c953
FaithLV <valters.tomsons@protonmail.com>
2019-09-21 17:08:28 +0300

    Add GameRegion enum

---
+++ src/SpectabisLib/Enums/GameRegion.cs
@@ -0,0 +1,16 @@
+namespace SpectabisLib.Enums
+{
+    public enum GameRegion
+    {
+        SLUS,
+        SCUS,
+        SCES,
+        SLES,
+        SCPS,
+        SLPS,
+        SLPM,
+        PSRM,
+        SCED,
+        SIPS
+    }
+}
