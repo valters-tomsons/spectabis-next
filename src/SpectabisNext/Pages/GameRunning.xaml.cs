7d07a685d6563d1e660b3509b2057556dc660abd
FaithLV <valters.tomsons@protonmail.com>
2020-01-19 23:51:02 +0200

    Add button to stop running game

--- src/SpectabisNext/Pages/GameRunning.xaml.cs
+++ src/SpectabisNext/Pages/GameRunning.xaml.cs
@@ -1,5 +1,6 @@
 using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using SpectabisLib.Interfaces;
 using SpectabisLib.Models;
@@ -11,21 +12,39 @@ namespace SpectabisNext.Pages
     {
         public string PageTitle { get; } = "PCSX2";
         public bool ShowInTitlebar { get; } = false;
-        public bool HideTitlebar { get; } = false;
+        public bool HideTitlebar { get; } = true;
         public bool ReloadOnNavigation { get; } = true;
 
+        private Button StopGame;
+
         private readonly IGameLauncher _gameLauncher;
+        private readonly IPageNavigationProvider _pageNavigation;
 
         [Obsolete("XAMLIL placeholder", true)]
         public GameRunning() { }
 
-        public GameRunning(IGameLauncher gameLauncher)
+        public GameRunning(IGameLauncher gameLauncher, IPageNavigationProvider pageNavigation)
         {
             _gameLauncher = gameLauncher;
+            _pageNavigation = pageNavigation;
+
             InitializeComponent();
+            RegisterChildren();
 
             var gameProc = _gameLauncher.GetRunningGame();
             Console.WriteLine($"[GameRunning] Game running:{gameProc.Game.Title} with processId '{gameProc.Process.Id}'");
+        }
+
+        private void RegisterChildren()
+        {
+            StopGame = this.FindControl<Button>("StopGame_Button");
+            StopGame.Click += StopGame_Click;
+        }
+
+        private void StopGame_Click(object sender, RoutedEventArgs e)
+        {
+            _gameLauncher.StopGame();
+            _pageNavigation.Navigate<GameLibrary>();
         }
 
         public void InitializeComponent()
