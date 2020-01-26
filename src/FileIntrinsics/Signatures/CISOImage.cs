0c2b5eb8a35509816acf19c8e33412812a4ee329
Valters <valters.tomsons@protonmail.com>
2020-01-26 05:38:21 +0200

    Introduce CSO image file signature

---
+++ src/FileIntrinsics/Signatures/CISOImage.cs
@@ -0,0 +1,25 @@
+using FileIntrinsics.Interfaces;
+
+namespace FileIntrinsics.Signatures
+{
+    public class CISOImage : IHeaderSignature
+    {
+        
+        public static IHeaderSignature Signature;
+
+        static CISOImage()
+        {
+            Signature = new CISOImage();
+        }
+
+        private readonly static byte[] _signature = {0x43, 0x49, 0x53, 0x4F};
+        private readonly static int[] _offsets = { 0x0 };
+        private readonly static string[] _extensions = {"cso"};
+
+        public byte[] ByteSignature => _signature;
+
+        public int[] Offsets => _offsets;
+
+        public string[] FileExtensions => _extensions;
+    }
+}
--- src/SpectabisLib/Services/GameProfileFactory.cs
+++ src/SpectabisLib/Services/GameProfileFactory.cs
@@ -20,9 +20,11 @@ namespace SpectabisLib.Services
             }
 
             var fileId = new IntrinsicsProvider();
+
             var isIsoTask = fileId.SignatureFound(gameFilePath, FileIntrinsics.Signatures.ISO9660.Signature);
             var iscueTask = fileId.SignatureFound(gameFilePath, FileIntrinsics.Signatures.CueDescription.Signature);
             var isBinTask = fileId.SignatureFound(gameFilePath, FileIntrinsics.Signatures.CD_I.Signature);
+            var isCsoTask = fileId.SignatureFound(gameFilePath, FileIntrinsics.Signatures.CISOImage.Signature);
 
             var profile = new GameProfile()
             {
@@ -32,10 +34,12 @@ namespace SpectabisLib.Services
             var isIso = await isIsoTask;
             var isCue = await iscueTask;
             var isBin = await isBinTask;
+            var isCso = await isCsoTask;
 
             System.Console.WriteLine($"isIso: {isIso}");
             System.Console.WriteLine($"isCue: {isCue}");
             System.Console.WriteLine($"isBin: {isBin}");
+            System.Console.WriteLine($"isCso: {isCso}");
 
             return profile;
         }
