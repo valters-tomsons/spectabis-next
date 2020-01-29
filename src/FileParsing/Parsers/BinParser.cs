3c9cf5b375cdb53c7f1b784ef05500e633a38cf0
Valters <valters.tomsons@protonmail.com>
2020-01-29 23:58:15 +0200

    Split file parsing logic to a FileParsing library

--- src/FileParsing/Parsers/BinParser.cs
+++ src/FileParsing/Parsers/BinParser.cs
@@ -4,9 +4,10 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SpectabisLib.Enums;
+using FileParsing.Enums;
+using FileParsing.Utils;
 
-namespace SpectabisLib.Services.Parsers
+namespace FileParsing.Parsers
 {
     public static class BinParser
     {
@@ -39,7 +40,7 @@ namespace SpectabisLib.Services.Parsers
 
             while(searching)
             {
-                var descPrefixStartIndex = ByteSearch(fileDescriptionBuffer, DescriptionPrefix, searchIndex);
+                var descPrefixStartIndex = ByteSearch.FindPattern(fileDescriptionBuffer, DescriptionPrefix, searchIndex);
 
                 if(descPrefixStartIndex == -1)
                 {
@@ -67,52 +68,5 @@ namespace SpectabisLib.Services.Parsers
         {
             return Encoding.UTF8.GetString(buffer).Replace(".", string.Empty);
         }
-
-        private static int ByteSearch(byte[] searchIn, byte[] searchBytes, int start = 0)
-        {
-            int found = -1;
-            //only look at this if we have a populated search array and search bytes with a sensible start
-            if (searchIn.Length > 0 && searchBytes.Length > 0 && start <= (searchIn.Length - searchBytes.Length) && searchIn.Length >= searchBytes.Length)
-            {
-                //iterate through the array to be searched
-                for (int i = start; i <= searchIn.Length - searchBytes.Length; i++)
-                {
-                    //if the start bytes match we will start comparing all other bytes
-                    if (searchIn[i] == searchBytes[0])
-                    {
-                        if (searchIn.Length > 1)
-                        {
-                            //multiple bytes to be searched we have to compare byte by byte
-                            bool matched = true;
-                            for (int y = 1; y <= searchBytes.Length - 1; y++)
-                            {
-                                if (searchIn[i + y] != searchBytes[y])
-                                {
-                                    matched = false;
-                                    break;
-                                }
-                            }
-                            //everything matched up
-                            if (matched)
-                            {
-                                found = i;
-                                break;
-                            }
-
-                        }
-                        else
-                        {
-                            //search byte is only one bit nothing else to do
-                            found = i;
-                            break; //stop the loop
-                        }
-
-                    }
-                }
-
-            }
-            return found;
-        }
-
     }
 }
