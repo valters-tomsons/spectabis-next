18f75c21f74c488a6ef3da1bae592c79dc3f7caa
Valters <valters.tomsons@protonmail.com>
2020-04-14 22:32:59 +0300

    Add game even if filetype parsing not supported

--- src/SpectabisLib/Services/GameProfileFactory.cs
+++ src/SpectabisLib/Services/GameProfileFactory.cs
@@ -26,12 +26,29 @@ namespace SpectabisLib.Services
             var gameSerial = await _gameParser.GetGameSerial(gameFilePath).ConfigureAwait(false);
             var metadata = _dbProvider.GetBySerial(gameSerial);
 
+            if(metadata == null)
+            {
+                var fileName = Path.GetFileName(gameFilePath);
+                System.Console.WriteLine($"[GameProfileFactory] Could not parse '{fileName}', using file name as game title");
+
+                return new GameProfile()
+                {
+                    FilePath = gameFilePath,
+                    Title = GetNameFromPath(gameFilePath)
+                };
+            }
+
             return new GameProfile()
             {
                 SerialNumber = gameSerial,
                 FilePath = gameFilePath,
                 Title = metadata.Title
             };
+        }
+
+        private string GetNameFromPath(string path)
+        {
+            return Path.GetFileNameWithoutExtension(path);
         }
     }
 }
