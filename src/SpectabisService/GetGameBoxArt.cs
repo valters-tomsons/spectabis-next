87e847471e286eb7f4ebb98ca055cbeab359b3d8
Valters <valters.tomsons@protonmail.com>
2020-05-15 04:23:09 +0300

    Implement game art caching in storage

--- src/SpectabisService/GetGameBoxArt.cs
+++ src/SpectabisService/GetGameBoxArt.cs
@@ -5,9 +5,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using SpectabisLib.Helpers;
 using SpectabisLib.Interfaces;
 using SpectabisService.Services;
-using SpectabisLib.Helpers;
 
 namespace SpectabisService
 {
@@ -17,6 +17,7 @@ namespace SpectabisService
         private static readonly IGameArtClient _artClient;
         private static readonly PCSX2DatabaseProvider _dbProvider = new PCSX2DatabaseProvider(_httpClient);
         private static readonly ContentDownloader _downloader = new ContentDownloader(_httpClient);
+        private static readonly StorageProvider _storage = new StorageProvider();
 
         static GetGameBoxArt()
         {
@@ -41,7 +42,7 @@ namespace SpectabisService
                 return new BadRequestObjectResult("Failed retrieving database");
             }
 
-            var normalizedSerial = ((string)reqSerial).NormalizeSerial();
+            var normalizedSerial = ((string) reqSerial).NormalizeSerial();
             var game = gameDb.FirstOrDefault(x => x.Serial.Equals(normalizedSerial));
 
             if (game == null)
@@ -49,8 +50,18 @@ namespace SpectabisService
                 return new BadRequestObjectResult("Unknown game");
             }
 
+            await _storage.InitializeStorage().ConfigureAwait(false);
+            var cached = await _storage.GetFromCache(normalizedSerial).ConfigureAwait(false);
+
+            if (cached != null)
+            {
+                return new FileContentResult(cached, "image/png");
+            }
+
             var artUrl = await _artClient.GetBoxArt(game.Title).ConfigureAwait(false);
             var result = await _downloader.DownloadGameArt(artUrl).ConfigureAwait(false);
+
+            await _storage.WriteToCache(normalizedSerial, result).ConfigureAwait(false);
 
             return new FileContentResult(result, "image/png");
         }
---
+++ src/SpectabisService/Services/StorageProvider.cs
@@ -0,0 +1,61 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Runtime;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace SpectabisService.Services
+{
+    public class StorageProvider
+    {
+        private const string ContainerName = "service-cache";
+
+        private readonly CloudStorageAccount storageAccount;
+        private CloudBlobClient client;
+        private CloudBlobContainer container;
+
+        public StorageProvider()
+        {
+            var conString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            storageAccount = CloudStorageAccount.Parse(conString);
+        }
+
+        public async Task InitializeStorage()
+        {
+            if(client != null)
+            {
+                return;
+            }
+
+            client = storageAccount.CreateCloudBlobClient();
+            container = client.GetContainerReference(ContainerName);
+            await container.CreateIfNotExistsAsync().ConfigureAwait(false);
+        }
+
+        public async Task<byte[]> GetFromCache(string serial)
+        {
+            var blob = container.GetBlockBlobReference(serial);
+            var exists = await blob.ExistsAsync().ConfigureAwait(false);
+
+            if(!exists)
+            {
+                return null;
+            }
+
+            await blob.FetchAttributesAsync().ConfigureAwait(false);
+
+            byte[] imageBuffer = new byte[blob.Properties.Length];
+            await blob.DownloadToByteArrayAsync(imageBuffer, 0).ConfigureAwait(false);
+
+            return imageBuffer;
+        }
+
+        public async Task WriteToCache(string serial, byte[] image)
+        {
+            var blob = container.GetBlockBlobReference(serial);
+            blob.Properties.ContentType = "image/png";
+            await blob.UploadFromByteArrayAsync(image, 0, image.Length).ConfigureAwait(false);
+        }
+    }
+}
