2ad169eb9068b1010abfe8f9f6ed847c6b07dbd7
Valters <valters.tomsons@protonmail.com>
2020-07-17 21:03:57 +0300

    Fix directory configuration assignment

--- src/SpectabisLib/Configuration/DirectoryStruct.cs
+++ src/SpectabisLib/Configuration/DirectoryStruct.cs
@@ -6,17 +6,28 @@ namespace SpectabisLib.Configuration
 {
     public class DirectoryStruct : IJsonConfig
     {
-        public DirectoryStruct()
-        {
-            PCSX2ConfigurationPath = new Uri(SystemDirectories.Default_PCSX2ConfigurationPath, UriKind.Absolute);
-            PCSX2Executable = new Uri(SystemDirectories.Default_PCSX2ExecutablePath, UriKind.Absolute);
-            LastFileBrowserDirectory = new Uri(SystemDirectories.HomeFolder, UriKind.Absolute);
-        }
+        private string _pcsx2Executable = SystemDirectories.Default_PCSX2ExecutablePath;
+        private string _pcsx2ConfigurationPath = SystemDirectories.Default_PCSX2ConfigurationPath;
+        private string _lastFileBrowserDirectory = SystemDirectories.HomeFolder;
 
         public string Title { get; } = "Directory";
 
-        public Uri PCSX2Executable { get; set; }
-        public Uri PCSX2ConfigurationPath { get; set; }
-        public Uri LastFileBrowserDirectory { get; set; }
+        public Uri PCSX2Executable
+        {
+            get { return new Uri(_pcsx2Executable, UriKind.Absolute); }
+            set { _pcsx2Executable = value.ToString(); }
+        }
+
+        public Uri PCSX2ConfigurationPath
+        {
+            get { return new Uri(_pcsx2ConfigurationPath, UriKind.Absolute); }
+            set { _pcsx2ConfigurationPath = value.ToString(); }
+        }
+
+        public Uri LastFileBrowserDirectory
+        {
+            get { return new Uri(_lastFileBrowserDirectory, UriKind.Absolute); }
+            set { _lastFileBrowserDirectory = value.ToString(); }
+        }
     }
 }
