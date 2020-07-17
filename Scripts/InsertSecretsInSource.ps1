18c7808be03eea67e20975a741dd0210e06eab4a
Valters <valters.tomsons@protonmail.com>
2020-07-17 20:52:16 +0300

    Properly tokenize API keys

--- Scripts/InsertSecretsInSource.ps1
+++ Scripts/InsertSecretsInSource.ps1
@@ -4,14 +4,12 @@ param(
 		[string]$targetFile
 	)
 
-$tokens = { "{{ServiceApiKey}}", "{{TelemetryKey}}" }
-$tokenValues = { $env:SERVICE_API_KEY, $env:TELEMETRY_KEY }
+$tokens = "{{ServiceApiKey}}", "{{TelemetryKey}}"
+$tokenValues = "$env:SERVICE_API_KEY", "$env:TELEMETRY_KEY"
 
+# Insert service API keys
 for ($i = 0; $i -lt $tokens.Count; $i++) {
-	(Get-Content "$targetFile") | foreach-object { $_ -replace $token[$i], $tokenValues[$i] } | Set-Content $targetFile
-	(Get-Content "$targetFile") | foreach-object { $_ -replace $tokens[$i], $tokenValue[$i] } | Set-Content $targetFile
+	(Get-Content "$targetFile") | foreach-object { $_ -replace $tokens[$i], $tokenValues[$i] } | Set-Content $targetFile
 }
 
-# Insert service API key
-(Get-Content "$targetFile") | foreach-object { $_ -replace $token, $tokenValue } | Set-Content $targetFile
-	Write-Host "Processed: " $targetFile
+Write-Host "Processed: " $targetFile
