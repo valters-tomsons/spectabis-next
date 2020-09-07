fd450aeee6d2cbc83647994d7dd7ec917bd2dbd2
Valters <valters.tomsons@protonmail.com>
2020-09-07 19:49:11 +0300

    Add test to check response content type

--- Tests/ServiceTests/Integration/GetGameBoxArtTest.cs
+++ Tests/ServiceTests/Integration/GetGameBoxArtTest.cs
@@ -10,6 +10,10 @@ namespace ServiceTests
     {
         private readonly IScenario Scenario;
 
+        private readonly string endpoint = "GetGameBoxArt";
+        private readonly string serialQuery = "serial";
+        private readonly string validSerial = "SLUS21376";
+
         public GetGameBoxArtTest()
         {
             var apiUrl = Environment.GetEnvironmentVariable("SERVICE_BASE_URL") ?? "http://localhost:7071/api/";
@@ -27,10 +31,20 @@ namespace ServiceTests
         public void When_requesting_game_art_should_return_OK()
         {
             // Act
-            Scenario.When.Get("GetGameBoxArt", "serial", "SLUS21376");
+            Scenario.When.Get(endpoint, serialQuery, validSerial);
 
             // Assert
             Scenario.Then.Response.ShouldBe.Ok();
+        }
+
+        [Fact]
+        public void When_requesting_game_art_should_return_image_content()
+        {
+            // Act
+            Scenario.When.Get(endpoint, serialQuery, validSerial);
+
+            // Assert
+            Scenario.Then.Response.Headers.Should.Include.ContentType("image/png");
         }
     }
 }
