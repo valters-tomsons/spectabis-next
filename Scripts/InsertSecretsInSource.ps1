13eeab7a76999704499b546edd9ab1d7efbaf164
Valters <valters.tomsons@protonmail.com>
2020-10-18 01:09:05 +0300

    Write tokenized result

--- Scripts/InsertSecretsInSource.ps1
+++ Scripts/InsertSecretsInSource.ps1
@@ -12,4 +12,5 @@ foreach($var in $env)
 	$targetContent = $targetContent.Replace("{{$($var.Name)}}", $var.Value)
 }
 
+Set-Content -Path $targetFile -Value $targetContent
 Write-Host "Finished processing $targetFile" -ForegroundColor Green
