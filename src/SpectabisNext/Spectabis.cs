eb26c159c4c209fa382b469d2f88cd2433b38d46
Valters <valters.tomsons@protonmail.com>
2021-05-08 15:13:00 +0300

    Remove faulty file version checking

--- src/SpectabisNext/Spectabis.cs
+++ src/SpectabisNext/Spectabis.cs
@@ -1,7 +1,4 @@
-using System.Diagnostics;
-using System.Reflection;
 using Avalonia.Controls;
-using SpectabisLib.Repositories;
 using SpectabisUI.Views;
 using SpectabisUI.Interfaces;
 
@@ -26,16 +23,7 @@ namespace SpectabisNext
 
         public string GetVersion()
         {
-            var assembly = Assembly.GetEntryAssembly();
-            var fileInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
-            var fileVersion = fileInfo.FileVersion;
-
-            if(fileVersion == "0.0.0.0")
-            {
-                return "develop";
-            }
-
-            return fileInfo.FileVersion;
+            return "develop";
         }
     }
 }
