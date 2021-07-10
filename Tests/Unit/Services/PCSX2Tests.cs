a39898675e75255ecb308c7cb1a099c0f1177ae0
Valters <valters.tomsons@protonmail.com>
2021-07-10 04:13:17 +0300

    Add unit test for PCSX2 launching

--- Tests/Unit/Services/PCSX2Tests.cs
+++ Tests/Unit/Services/PCSX2Tests.cs
@@ -2,33 +2,51 @@ using System;
 using System.Threading.Tasks;
 using AutoFixture;
 using AutoFixture.AutoMoq;
+using Moq;
+using SpectabisLib.Enums;
 using SpectabisLib.Interfaces.Abstractions;
 using SpectabisLib.Models;
 using SpectabisLib.Services;
 using Xunit;
+using Unit.Helpers;
 
 namespace Unit.Services
 {
     public class PCSX2Tests
     {
+        private readonly IProfileFileSystem _pfsMock;
+
         private readonly GameLauncherPCSX2 _service;
 
         public PCSX2Tests()
         {
             var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            _pfsMock = fixture.Freeze<IProfileFileSystem>();
+
             _service = fixture.Create<GameLauncherPCSX2>();
         }
 
-        [Fact]
-        public async Task Test()
+        [UnixOnlyFact]
+        public async Task GivenProfileValid_StartGameCalled_ReferencesCorrectFilesAndConfigArgument()
         {
-            var game = new GameProfile() { Id = Guid.NewGuid() };
+            Mock.Get(_pfsMock).Setup(x => x.GetContainerUri(It.IsAny<GameProfile>(), ContainerConfigType.Inis)).Returns(new Uri("/"));
 
-            var actual = await _service.StartGame(game);
-            actual.GetProcess().Kill(true);
+            var game = new GameProfile()
+            {
+                Id = Guid.NewGuid(),
+                EmulatorPath = "TestData/PCSX2Stub",
+                FilePath = "TestData/game.fake",
+            };
+
+            var actual = await _service.StartGame(game).ConfigureAwait(false);
+            var stubOutput = await actual.GetProcess().StandardOutput.ReadToEndAsync().ConfigureAwait(false);
 
             Assert.Equal(game.Id, actual.GetGame().Id);
+            Assert.Equal(game.EmulatorPath, actual.GetProcess().StartInfo.FileName);
             Assert.NotNull(actual.GetProcess());
+
+            Assert.Contains(game.FilePath, stubOutput);
+            Assert.Contains("--cfgpath=", stubOutput);
         }
     }
 }
