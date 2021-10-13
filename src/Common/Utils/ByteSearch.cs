b13a46ff2caa3244abb3a9ce23c3d8dc0811ea9a
Valters <valters.tomsons@protonmail.com>
2021-10-13 23:19:22 +0300

    Add documentation for ByteSearch

--- src/Common/Utils/ByteSearch.cs
+++ src/Common/Utils/ByteSearch.cs
@@ -2,25 +2,29 @@ namespace Common.Utils
 {
     public static class ByteSearch
     {
-        public static int FindPattern(byte[] searchIn, byte[] searchBytes, int start = 0)
+        /// <summary>
+        /// Returns index of provided byte pattern in a buffer,
+        /// returns -1 if not found
+        /// </summary>
+        public static int FindPattern(byte[] buffer, byte[] searchPattern, int offset = 0)
         {
             int found = -1;
             //only look at this if we have a populated search array and search bytes with a sensible start
-            if (searchIn.Length > 0 && searchBytes.Length > 0 && start <= (searchIn.Length - searchBytes.Length) && searchIn.Length >= searchBytes.Length)
+            if (buffer.Length > 0 && searchPattern.Length > 0 && offset <= (buffer.Length - searchPattern.Length) && buffer.Length >= searchPattern.Length)
             {
                 //iterate through the array to be searched
-                for (int i = start; i <= searchIn.Length - searchBytes.Length; i++)
+                for (int i = offset; i <= buffer.Length - searchPattern.Length; i++)
                 {
                     //if the start bytes match we will start comparing all other bytes
-                    if (searchIn[i] == searchBytes[0])
+                    if (buffer[i] == searchPattern[0])
                     {
-                        if (searchIn.Length > 1)
+                        if (buffer.Length > 1)
                         {
                             //multiple bytes to be searched we have to compare byte by byte
                             bool matched = true;
-                            for (int y = 1; y <= searchBytes.Length - 1; y++)
+                            for (int y = 1; y <= searchPattern.Length - 1; y++)
                             {
-                                if (searchIn[i + y] != searchBytes[y])
+                                if (buffer[i + y] != searchPattern[y])
                                 {
                                     matched = false;
                                     break;
