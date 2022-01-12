bfee086215bcad2bc6bd3c210a029306cc5eb16c
Valters <valters.tomsons@protonmail.com>
2022-01-12 03:22:26 +0200

    Undefined to Automatic GSRenderer type

--- src/EmuConfig/Enums/GSRendererType.cs
+++ src/EmuConfig/Enums/GSRendererType.cs
@@ -3,15 +3,15 @@ namespace EmuConfig.Enums
     // https://github.com/PCSX2/pcsx2/blob/master/plugins/GSdx/GS.h#L231
     public enum GSRendererType
     {
-        Undefined = -1,
+        Automatic = -1,
 
         DX1011_HW = 3,
         DX1011_SW,
 
         Null = 11,
 
-        OGL_HW,
-        OGL_SW,
+        OGL_HW = 12,
+        OGL_SW = 13,
 
         DX1011_OpenCL = 15,
         OGL_OpenCL = 17,
