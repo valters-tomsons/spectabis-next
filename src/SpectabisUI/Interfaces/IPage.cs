ea3f3b183e4d632aca5ed8546062548091bf9fc5
Valters Tomsons <valters.tomsons@protonmail.com>
2022-01-22 01:04:39 +0200

    Remove comments

--- src/SpectabisUI/Interfaces/IPage.cs
+++ src/SpectabisUI/Interfaces/IPage.cs
@@ -3,14 +3,8 @@ namespace SpectabisUI.Interfaces
     public interface IPage
     {
         string PageTitle { get; }
-
-        //replace with "InTitlebar"
         bool ShowInTitlebar { get; }
-
-        //replace with "Hides"
         bool HideTitlebar { get; }
-
-        //replace with "Keep in memory after"
         bool ReloadOnNavigation { get; }
 
         void InitializeComponent();
